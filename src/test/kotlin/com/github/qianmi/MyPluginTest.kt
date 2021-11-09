6836774ae8e511c9a44df44bb122f1b8d8231a20
刘富国 <595726017@qq.com>
2021-11-09 19:20:31 +0800

    feat: 增加 bugatti 项目配置

--- src/test/kotlin/com/github/qianmi/MyPluginTest.kt
+++ src/test/kotlin/com/github/qianmi/MyPluginTest.kt
@@ -1,28 +1,28 @@
-package com.github.qianmi
-
-import com.intellij.testFramework.TestDataPath
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
-
-@TestDataPath("\$CONTENT_ROOT/src/test/testData")
-class MyPluginTest : BasePlatformTestCase() {
-
-    fun testXMLFile() {
-//        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
-//        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
+//package com.github.qianmi
 //
-//        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
+//import com.intellij.testFramework.TestDataPath
+//import com.intellij.testFramework.fixtures.BasePlatformTestCase
 //
-//        assertNotNull(xmlFile.rootTag)
+//@TestDataPath("\$CONTENT_ROOT/src/test/testData")
+//class MyPluginTest : BasePlatformTestCase() {
 //
-//        xmlFile.rootTag?.let {
-//            assertEquals("foo", it.name)
-//            assertEquals("bar", it.value.text)
-//        }
-    }
-
-    override fun getTestDataPath() = "src/test/testData/rename"
-
-    fun testRename() {
-//        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
-}
+//    fun testXMLFile() {
+////        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
+////        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
+////
+////        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
+////
+////        assertNotNull(xmlFile.rootTag)
+////
+////        xmlFile.rootTag?.let {
+////            assertEquals("foo", it.name)
+////            assertEquals("bar", it.value.text)
+////        }
+//    }
+//
+//    override fun getTestDataPath() = "src/test/testData/rename"
+//
+//    fun testRename() {
+////        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
+//    }
+//}
